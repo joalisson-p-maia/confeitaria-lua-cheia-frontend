1f843fd3eec6b6d78c185400bb0d9746ba7bb4f1
Namibo06 <namibo654@gmail.com>
2025-11-23 01:31:30 -0300

    refactor: alterando input por select

--- src/app/pages/estoque-movimentacao-create/estoque-movimentacao-create.ts
+++ src/app/pages/estoque-movimentacao-create/estoque-movimentacao-create.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { EstoqueService } from '../../services/estoque';
 import { Header } from '../../shared/header/header';
 import { Footer } from '../../shared/footer/footer';
+import { InsumosService } from '../../services/insumos';
 
 @Component({
   selector: 'app-estoque-movimentacao-create',
@@ -18,12 +19,26 @@ import { Footer } from '../../shared/footer/footer';
 })
 export class EstoqueMovimentacaoCreate {
   form = {
-    insumoId: 0,
+    nome: "",
     quantidade: 0,
     tipo: 'entrada'
   };
+  listaInsumos: any = [];
 
-  constructor(private service: EstoqueService) {}
+  constructor(
+    private service: EstoqueService,
+    private InsumoService: InsumosService
+  ) {}
+
+  ngOnInit(): void {
+    this.InsumoService.listar().subscribe({
+      next: (res) => {
+        console.log(res);
+
+        this.listaInsumos = res;
+      }
+    });
+  }
 
   salvar() {
     this.service.registrarMovimentacao(this.form).subscribe({
